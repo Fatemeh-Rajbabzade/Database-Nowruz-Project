18d5ade4babf4249388143d44e161fd8182663ac
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-04-20 10:02:10 -0700

    Creat Database class

---
+++ src/db/Database.java
@@ -0,0 +1,45 @@
+package db;
+
+import db.exception.EntityNotFoundException;
+
+import java.util.ArrayList;
+
+public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int currentId = 10;
+
+    //methods
+    //e موجودیت است
+    public static void add(Entity e){
+      e.id = currentId ++;
+        entities.add(e);
+    }
+
+    public static Entity get(int id) throws EntityNotFoundException{
+        for (Entity e : entities){
+            if (e.id == id)
+                return e;
+        }
+        throw new EntityNotFoundException(id);
+
+    }
+
+    public static void delete(int id) throws EntityNotFoundException{
+        for (Entity e : entities){
+            if (e.id == id)
+            entities.remove(e);
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static update(Entity e) throws EntityNotFoundException{
+        for (Entity e : entities){
+            if (e.id == id)
+
+
+        }
+    }
+
+
+
+}
