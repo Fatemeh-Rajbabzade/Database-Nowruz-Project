26254e6f08abee51afe24e39dda83c8f50c8fb82
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-05-03 01:13:17 -0700

    Validate in add and update methods

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,19 +1,35 @@
 package db;
 
 import db.exception.EntityNotFoundException;
+import db.exception.InvalidEntityException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int currentId = 1;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
     private Database() {
     }
 
+    //method for validator
+    public static void registerValidator(int entityCode, Validator validator) {
+        if(validators.containsKey(entityCode)){
+            throw new IllegalArgumentException("This validator already registered with entity code: " + entityCode);
+        }
+        validators.put(entityCode, validator);
+    }
+
     //methods
     //e موجودیت است
-    public static void add(Entity e) {
+    public static void add(Entity e) throws InvalidEntityException{
+        //موجودیت رو اعتیار سنجی کند
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null) {
+            validator.validate(e);
+        }
         e.id = currentId++;
         //کپی کردن entity
         Entity copyEntity = e.copy();
@@ -40,10 +56,14 @@ public class Database {
         throw new EntityNotFoundException(id);
     }
 
-    public static void update(Entity e) throws EntityNotFoundException {
+    public static void update(Entity e) throws EntityNotFoundException , InvalidEntityException{
         //کپی کردن
         Entity copyEntity = e.copy();
-
+        // ولیدیتور برای موجودیت
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null) {
+            validator.validate(e);
+        }
         int entitiesLength = entities.size();
         for (int i = 0; i < entitiesLength; i++) {
             if (entities.get(i).id == copyEntity.id) {
