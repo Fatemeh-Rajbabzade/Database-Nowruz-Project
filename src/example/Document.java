362e7f92adf15fdb9232c036aab44ae65f074448
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-05-03 03:08:00 -0700

    Add Document class.

---
+++ src/example/Document.java
@@ -0,0 +1,56 @@
+package example;
+
+import db.Database;
+import db.Entity;
+import db.Trackable;
+import java.util.Date;
+
+public class Document extends Entity implements Trackable {
+    private String content;
+    private Date creationDate;
+    private Date lastModificationDate;
+
+    public Document(String content){
+        this.content = content;
+    }
+
+    //methods
+
+    @Override
+    public int getEntityCode() {
+        return 14;
+    }
+
+    @Override
+    public Document copy(){
+        Document copyOfDocument = new Document(content);
+        copyOfDocument.id = id;
+        copyOfDocument.creationDate = creationDate;
+        copyOfDocument.lastModificationDate = lastModificationDate;
+        return copyOfDocument;
+
+    }
+
+    @Override
+   public void setCreationDate(Date date){
+        this.creationDate = date;
+    }
+
+    @Override
+    public Date getCreationDate(){
+        return creationDate;
+    }
+
+    @Override
+    public  void setLastModificationDate(Date date){
+        this.lastModificationDate = date;
+    }
+
+    @Override
+    public Date getLastModificationDate(){
+        return lastModificationDate;
+    }
+
+
+
+}
