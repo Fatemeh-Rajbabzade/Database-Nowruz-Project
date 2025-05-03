dfd9d44f73a878f0f1cec8640fd1575fcd6c1141
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-05-02 23:06:23 -0700

    Add HumanValidator to check the type of Entity.

--- src/example/HumanValidator.java
+++ src/example/HumanValidator.java
@@ -1,4 +1,23 @@
 package example;
 
-public class HumanValidator {
+import db.exception.InvalidEntityException;
+import db.Validator;
+import db.Entity;
+
+public class HumanValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        // Code for human validation
+
+        if (! (entity instanceof Human))
+            throw new IllegalArgumentException("Type of Entity must be Human!");
+
+        Human human = (Human) entity;
+        if(human.name == null || human.name.isEmpty())
+            throw new InvalidEntityException("Name Cannot be null or empty.");
+
+        if (human.age < 0)
+            throw new InvalidEntityException("Age Cannot be negative.");
+
+    }
 }
